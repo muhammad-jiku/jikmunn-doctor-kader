872ce2a58781582d461312af4e4cf27ce1f7f4b2
muhammad-jiku <muhammadjiku364@gmail.com>
2022-04-18 12:21:21 +0600

    about section added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,11 +1,26 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
+import pic from '../../Images/pic_1.jpg';
 
 const About = () => {
   return (
-    <Container fluid>
-      <h3>This is About page </h3>
-    </Container>
+    <>
+      <Container fluid>
+        <img src={pic} alt="" />
+        <p>
+          Hi! My name is Muhammad Azizul Hoque Jiku. Welcome to my personal page
+          that I've designed to showcase my skills and expertise that I've
+          accumulated over the year. I've also placed all my social links at the
+          left side, feel free to connect with me. I'd love to be given the
+          opportunity to hear from you. I like to call myself “a passionate
+          computer science teacher”. I always had a wish on my mind that I will
+          do something for the computer science students that's why I made lots
+          of videos on different subjects. My first priority is making the
+          subject more interesting and clear so you can feel it and don't get
+          bore.
+        </p>
+      </Container>
+    </>
   );
 };
 
