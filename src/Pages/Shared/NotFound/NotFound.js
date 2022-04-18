cbc96f227ea9a38dbd605aa864bae6c0130921f9
muhammad-jiku <muhammadjiku364@gmail.com>
2022-04-18 21:03:34 +0600

    404 page designed

--- src/Pages/Shared/NotFound/NotFound.js
+++ src/Pages/Shared/NotFound/NotFound.js
@@ -1,10 +1,22 @@
 import React from 'react';
+import { Container, Image } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import Error from '../../../Images/pikabu.gif';
+import './NotFound.css';
 
 const NotFound = () => {
   return (
-    <div>
-      <h3>Page not found</h3>
-    </div>
+    <Container>
+      <div className="showError">
+        <Image src={Error} alt="" fluid />
+        <h4>
+          You're lost dear go back to the{' '}
+          <Link to="/" className="showHome">
+            Home
+          </Link>{' '}
+        </h4>
+      </div>
+    </Container>
   );
 };
 
