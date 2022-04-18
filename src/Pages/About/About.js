7de1959485ce51739ca482f85fcba5ec336fed12
muhammad-jiku <muhammadjiku364@gmail.com>
2022-04-18 20:17:53 +0600

    about section added

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,26 +1,30 @@
 import React from 'react';
 import { Container } from 'react-bootstrap';
 import pic from '../../Images/pic_1.jpg';
+import './About.css';
 
 const About = () => {
   return (
-    <>
-      <Container fluid>
-        <img src={pic} alt="" />
-        <p>
-          Hi! My name is Muhammad Azizul Hoque Jiku. Welcome to my personal page
-          that I've designed to showcase my skills and expertise that I've
-          accumulated over the year. I've also placed all my social links at the
-          left side, feel free to connect with me. I'd love to be given the
-          opportunity to hear from you. I like to call myself “a passionate
-          computer science teacher”. I always had a wish on my mind that I will
-          do something for the computer science students that's why I made lots
-          of videos on different subjects. My first priority is making the
-          subject more interesting and clear so you can feel it and don't get
-          bore.
-        </p>
-      </Container>
-    </>
+    <Container>
+      <div className="aboutSection">
+        <div className="aboutSectionLeft">
+          <img src={pic} alt="" />
+        </div>
+        <div className="aboutSectionRight">
+          <p>
+            Hi! My name is{' '}
+            <span className="aboutSectionRightName">
+              Muhammad Azizul Hoque Jiku
+            </span>
+            . Welcome to Dr.Abdul Kader's personal site that I've designed. I'd
+            love to be fullstack web-developer. I'd like to call myself "a
+            passionate day dreamer". Right now, I am learning about
+            web-development. My dreams, goals all set to be the fullstack
+            web-developer.
+          </p>
+        </div>
+      </div>
+    </Container>
   );
 };
 
